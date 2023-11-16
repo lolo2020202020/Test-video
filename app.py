885c72361d1fe5c71543c298629e7d009760d26f
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-16 22:00:45 +0100

    change

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from Feature_01 import return_even
 from Feature_02 import return_odd
 
@@ -7,7 +6,7 @@ original_list = [i for i in range(10)]
 
 even_list = return_even(original_list)
 
-odd_list = return_odd
+odd_list = return_odd(original_list)
 
 st.write("Hooray, we connected everything")
 
