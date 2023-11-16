0950b6d8368784fb0a8af5545e0338b120020178
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-16 21:51:24 +0100

    create new feature

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+from Feature_01 import return_even
+
+original_list = [i for i in range(10)]
+
+even_list = return_even(original_list)
+
+
+
 st.write("Hooray, we connected everything")
 
 st.write("Ciao")
